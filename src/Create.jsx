c5d63ec9f3cdbbbf95d09442a61b9ba43afdd5fb
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-10-27 00:06:56 +0530

    Modified src|Create.js

--- src/Create.jsx
+++ src/Create.jsx
@@ -11,7 +11,7 @@ function Create() {
     return(
         <div className="create_form">
          <input type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value)}/>
-         <button type="button" onclick={handleAdd}>Add</button>
+         <button type="button" onClick={handleAdd}>Add</button>
         </div>
     )
 }
