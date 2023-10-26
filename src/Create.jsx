f6c708ffa9c26087b23e29457b84c303ad9af31e
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-10-26 19:12:36 +0530

    Modified src|Create/Home

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import axios from 'axios' ;
+import axios from 'axios';
+
 function Create() {
     const [task, setTask] = useState();
     const handleAdd =() => {
-       axios.post('http://localhost:3001/add', {task:task})
+       axios.post('http://localhost:3001/add', {task: task})
        .then(result => console.log(result))
        .catch(err => console.log(err))
     }
     return(
-        <div>
+        <div className="create_form">
          <input type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value)}/>
          <button type="button" onclick={handleAdd} class="btn btn-info" >Add</button>
         </div>
